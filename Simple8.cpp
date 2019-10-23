9645acf5d246a4e5d36a39b61cb36c5d6897a931
jcshim <jcshim@andong.ac.kr>
2019-10-24 07:41:53 +0900

    단계별 처리

--- Simple8.cpp
+++ Simple8.cpp
@@ -1,25 +1,49 @@
 #include<afxwin.h>
-class CMyFrm : public CFrameWnd {
+class CMainFrame : public CFrameWnd {
 public:
-	CMyFrm() {
-		Create(NULL, L"Hi !");
-	}
+	CMainFrame() { Create(NULL, L"Hi !"); }
 	DECLARE_MESSAGE_MAP()
-	void OnPaint();
+	afx_msg void OnPaint();
 };
-class CMy : public CWinApp {
-	BOOL InitInstance() {
-		CMyFrm* Frm = new CMyFrm();
+class CMyApp : public CWinApp {
+public:
+	virtual BOOL InitInstance() {
+		CMainFrame* Frm = new CMainFrame();
 		m_pMainWnd = Frm;
-		Frm->ShowWindow(1);
-		return TRUE;
+		Frm->ShowWindow(TRUE);
+		return CWinApp::InitInstance();
 	}
 };
-CMy theApp;
-BEGIN_MESSAGE_MAP(CMyFrm, CFrameWnd)
+CMyApp theApp;
+BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd)
 	ON_WM_PAINT()
 END_MESSAGE_MAP()
-void CMyFrm::OnPaint(){
-	CClientDC dc(this);
-	dc.Ellipse(100, 100, 200, 200);
+
+void CMainFrame::OnPaint()
+{
+	CPaintDC dc(this);
+	dc.Rectangle(20, 20, 80, 80);					  
 }
+/*
+CMy theApp;
+// 1. 틀을 만든다.
+class CMainFrmae : public CFrameWnd {
+};
+class CMyApp : public CWinApp {
+};
+CMyApp theApp;
+// 2. CMyApp::InitInstance()를 오버라이딩하고
+  CMainFrame* Frm = new CMainFrame();
+  m_pMainWnd = Frm;
+  Frm->ShowWindow(TRUE);
+// 3. 생성자에 Create()추가
+public:
+	CMainFrame() { Create(NULL, L"Hi !"); }
+// 4. 실행테스트
+// 5. CMainFrame에
+DECLARE_MESSAGE_MAP()
+// 6. 외부에
+BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd)
+END_MESSAGE_MAP()
+//7. CMainFrame에 WM_PAINT를 구현
+*/
